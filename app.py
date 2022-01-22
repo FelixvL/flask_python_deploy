484e4a1526142d39c0d1256d31b83e500a9960d1
felixvl <digitaleservice@gmail.com>
2022-01-22 22:24:31 +0100

    start met flask

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from externebestanden import bestand1 as been
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():    
+    return render_template("index.html")
+
+@app.route("/een")
+def methodeEen():
+    terug = been.eenmethodeinbestand1()
+    return "methodeEen"+terug
+
+@app.route("/twee")
+def methodeTwee():
+    terug = been.methodeMetReadFile()
+    return "methodeEen"+terug
+
+if __name__ == '__main__':
+    app.run(debug=True)
