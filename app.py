71b7d84028c763ae2f608b76e2cc82babce0f521
felixvl <digitaleservice@gmail.com>
2022-01-22 23:18:32 +0100

    veel eruit

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask
+#, render_template, request
 #from externebestanden import bestand1 as been
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():    
-    return render_template("index.html")
+    return "index.html"
 
 
 
