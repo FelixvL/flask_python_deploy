1579f82d4ae6390f65024e19cba581ef4048bb68
felixvl <digitaleservice@gmail.com>
2022-01-22 23:21:28 +0100

    render erbij

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
-#, render_template, request
+from flask import Flask, render_template
 #from externebestanden import bestand1 as been
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():    
-    return "index.html"
+    return render_template("index.html")
 
 
 
