bc51674f3538537d853f099d12dbba834b0dda0d
felixvl <digitaleservice@gmail.com>
2022-01-22 23:15:09 +0100

    externe file eruit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from externebestanden import bestand1 as been
+#from externebestanden import bestand1 as been
 
 app = Flask(__name__)
 
@@ -7,13 +7,5 @@ app = Flask(__name__)
 def index():    
     return render_template("index.html")
 
-@app.route("/een")
-def methodeEen():
-    terug = been.eenmethodeinbestand1()
-    return "methodeeen "+terug
 
-@app.route("/twee")
-def methodeTwee():
-    terug = been.methodeMetReadFile()
-    return "methodeEen"+terug
 
