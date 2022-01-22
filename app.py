d04cf8bfebf7206d32c6a7941e1aec8669c9a08d
felixvl <digitaleservice@gmail.com>
2022-01-22 23:24:41 +0100

    route erbij

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-#from externebestanden import bestand1 as been
+from externebestanden import bestand1 as been
 
 app = Flask(__name__)
 
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():    
     return render_template("index.html")
 
+@app.route("/een")
+def methodeEen():
+    terug = been.eenmethodeinbestand1()
+    return "methodeeen "+terug
 
+#@app.route("/twee")
+#def methodeTwee():
+#    terug = been.methodeMetReadFile()
+#    return "methodeEen"+terug
 
