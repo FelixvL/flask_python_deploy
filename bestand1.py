211b224a93c2126228ad1b2e2037cc1b4e4574b0
felixvl <digitaleservice@gmail.com>
2022-01-23 11:12:52 +0100

    met pandas

--- bestand1.py
+++ bestand1.py
@@ -1,13 +1,13 @@
-#import pandas as pd
+import pandas as pd
 
 def eenmethodeinbestand1():
     return "dit komt uit methode 1"
 
 
 def methodeMetReadFile():
-    #pokemons = pd.read_csv("externebestanden/Pokemon.csv")
-    #kolomnaam = pokemons.columns[4] 
-    return "kolomnaam"
+    pokemons = pd.read_csv("Pokemon.csv")
+    kolomnaam = pokemons.columns[4] 
+    return kolomnaam
 
 
 #methodeMetReadFile()
